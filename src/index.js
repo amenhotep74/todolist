00d275c275d5b4de4687df62b6aca2cab749b215
unknown <Blewie144@gmail.com>
2020-01-09 16:11:29 +1030

    Added delete button

--- src/index.js
+++ src/index.js
@@ -21,21 +21,42 @@ addBookToArray(new Todo("Walk Dog", "description", "1/3/2018", "default"));
 console.log(Todo);
 console.log(arrayDefault);
 
+// to retrieve all info about an object 
+Todo.prototype.info = function() {
+    return `${this.title}`;
+}
 
 // for each object in the array export to an li on the page 
-arrayDefault.forEach(function (arrayItem) {
+function renderTodo() {
     var demo = document.querySelector("#demo");
-    demo.dataset.index = index;
-    var ul = document.createElement("ul");
-    demo.appendChild(ul);
-    let x = arrayItem.title;
-    var li = document.createElement("li");
+    demo.innerHTML = "";
+    const todos = arrayDefault.map((todo, index) => createLi(todo, index));
+    todos.forEach(todo => demo.appendChild(todo));
+}
+
+function createLi(todo, index) {
+    const ul = document.createElement('ul');
+    ul.dataset.index = index;
+    const li = document.createElement('li');
+    li.innerHTML = todo.info();
     ul.appendChild(li);
-    li.innerHTML = x + ' ' + `${arrayItem.title} <button id="delete">Delete</button>`;
-    console.log(x);
-    // add event listener for button 
-    document.getElementById("delete").addEventListener('click', deleteTodo);
-})
+
+    // create delete button 
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "DELETE";
+    deleteButton.onclick = () => {
+        removeTodoFromArray(index);
+        // re render todo
+        renderTodo();
+    };
+    li.appendChild(deleteButton);
+    return ul;
+}
+
+// from the object from the array
+function removeTodoFromArray(index) {
+    arrayDefault.splice(index, 1);
+}
 
 // When todo delete button is clicked on 
 function deleteTodo(ev) {
@@ -44,4 +65,4 @@ function deleteTodo(ev) {
     console.log(ev.currentTarget);
 }
 
-// when new todo is created 
+renderTodo();
